f7aea5dd0b25e47083e16b625b72c8a7a0522e3f
Muhammad Saif <hello@localhost.localdomain>
2022-08-03 19:11:27 +0500

    Fixed Bugs and added Time Delay

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const access_token = 'd0c6e78050da4f11b85bc92aeb21887d';
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
+/**
+ * Get Active Project Return Project Data of Currently
+ * Processing Status.
+ * @param {string} access_token_use 
+ * @returns {object} Project Data
+ */
 async function getActiveProject(access_token_use) {
     try {
         const response = await axios.post('http://webautoma.pk/api/project/get', {
@@ -17,6 +23,12 @@ async function getActiveProject(access_token_use) {
     }
 }
 
+/**
+ * Get Active Project Task Data
+ * @param {int} project_id 
+ * @param {string} access_token_use 
+ * @returns {object} Task Data Object
+ */
 
 async function getTaskData(project_id, access_token_use) {
     try {
@@ -31,6 +43,12 @@ async function getTaskData(project_id, access_token_use) {
         console.error(error);
     }
 }
+
+/**
+ * Get Proxy Data from Server.
+ * @param {string} access_token_use 
+ * @returns {object} Proxy Data Object
+ */
 async function getproxy(access_token_use) {
     try {
         const response = await axios.post('http://webautoma.pk/api/proxy/get', {
@@ -44,6 +62,14 @@ async function getproxy(access_token_use) {
     }
 }
 
+/**
+ * Update Task Status Method
+ * @param {string} access_token_use 
+ * @param {string} status 
+ * @param {string} log_msg 
+ * @param {int|string} task_id 
+ * @returns {object} Update Status Message Got from Server
+ */
 async function updateTaskStatus(access_token_use, status, log_msg, task_id) {
     try {
         const response = await axios.post('http://webautoma.pk/api/project/updateTaskStatus', {
@@ -74,8 +100,101 @@ async function getTotalTasks(access_token_use, project_id) {
     }
 }
 
-async function main(access_token) {
-    var project = await getActiveProject(access_token);
+async function taskProcess(task, access_token, proxy, args) {
+    if (task) {
+        let puperr;
+        const browser = await puppeteer.launch(args).catch(err => {
+            puperr = err
+        });
+        if (puperr) {
+            var logs = await updateTaskStatus(access_token, 'error', puperr.toString(), task.task_id);
+        }
+        try {
+            console.log('Running Task Of Project..')
+            const page = await browser.newPage();
+            if (proxy.is_proxy_auth === 'yes') {
+
+                await page.authenticate({
+                    p_username,
+                    p_password
+                });
+                console.log('Authenticated!!!');
+            }
+            await page.goto(task.project_url, {
+                waitUntil: 'networkidle2',
+            })
+            // await page.waitForTimeout();
+            // console.log(task.task_data);
+            var errors;
+            await page.waitForSelector(task.task_data[0].selector, {
+                visible: true
+            });
+            for (let d of task.task_data) {
+                // console.log(d);
+                await page.waitForTimeout(1000);
+
+                switch (d.input_type) {
+                    case 'input':
+                        // console.log(d.selector_type);
+                        await page.type(d.selector, d.value).catch(err => {
+                            errors = err;
+                        });
+                        break;
+                    case 'submit':
+                        await page.click(d.selector).catch(err => {
+                            errors = err;
+                        });
+                        break;
+                    case 'checkbox':
+                        await page.click(d.selector, {
+                            clickCount: 1
+                        }).catch(err => {
+                            errors = err;
+                        });
+                        break;
+                    case 'radiobtn':
+                        await page.click(d.selector, {
+                            clickCount: 1
+                        }).catch(err => {
+                            errors = err;
+                        });
+                        break;
+                    case 'select':
+                        await page.select(d.selector, d.value).catch(err => {
+                            errors = err;
+                        });
+                        break;
+                    case 'button':
+                        await page.click(d.selector).catch(err => {
+                            errors = err;
+                        });
+                        break;
+                }
+                if (errors) {
+                    console.log('Log Error BREAK.');
+                    break;
+                }
+            }
+            if (errors) {
+                var logs = await updateTaskStatus(access_token, 'error', errors.toString(), task.task_id);
+                // var log=await Promise.all(logser);
+
+            } else {
+                var logs = await updateTaskStatus(access_token, 'completed', 'Task Completed Successfully.', task.task_id);
+            }
+            await page.waitForTimeout(10000);
+
+            // console.log(logs);
+            await browser.close();
+            return logs;
+            // console.log(`All done, check the screenshot. ✨`)
+        } catch (error) {
+            var logs = await updateTaskStatus(access_token, 'error', error.toString(), task.task_id);
+            return logs;
+        }
+    }
+}
+async function main(access_token, project) {
     if (project != null || project != '' || project != 0) {
         // var proxy = await getproxy();
         var proxy = await getproxy(access_token);
@@ -85,114 +204,32 @@ async function main(access_token) {
                 proxy_url = proxy.proxy_url;
                 var args = {
                     args: [`--proxy-server=${proxy_url}`],
-                    headless: true
+                    headless: false
                 }
                 var p_username = proxy.proxy_username;
                 var p_password = proxy.proxy_password;
                 console.log('Using Proxy....');
             } else {
                 var args = {
-                    headless: true
+                    headless: false
                 }
             }
             var task = await getTaskData(project, access_token);
-            // console.log(task);
-            // var puperr='Puppeter Error Contact your Developer';
-            if (task) {
-              var puperr;
-                puppeteer.launch(args).then(async browser => {
-                    console.log('Running tests..')
-                    const page = await browser.newPage();
-                    if (proxy.is_proxy_auth === 'yes') {
-
-                        await page.authenticate({
-                            p_username,
-                            p_password
-                        });
-                        console.log('Authenticated!!!');
-                    }
-                    await page.goto(task.project_url, {
-                        waitUntil: 'networkidle2',
-                    })
-                    // await page.waitForTimeout();
-                    // console.log(task.task_data);
-                    var errors;
-                    for (let d of task.task_data) {
-                        // console.log(d);
-                        await page.waitForTimeout(1000);
-
-                        switch (d.input_type) {
-                            case 'input':
-                                // console.log(d.selector_type);
-                                await page.type(d.selector, d.value).catch(err => {
-                                    errors = err;
-                                });
-                                break;
-                            case 'submit':
-                                await page.click(d.selector).catch(err => {
-                                    errors = err;
-                                });
-                                break;
-                            case 'checkbox':
-                                await page.click(d.selector, {
-                                    clickCount: 1
-                                }).catch(err => {
-                                    errors = err;
-                                });
-                                break;
-                            case 'radiobtn':
-                                await page.click(d.selector, {
-                                    clickCount: 1
-                                }).catch(err => {
-                                    errors = err;
-                                });
-                                break;
-                            case 'select':
-                                await page.select(d.selector, d.value).catch(err => {
-                                    errors = err;
-                                });
-                                break;
-                            case 'button':
-                                await page.click(d.selector).catch(err => {
-                                    errors = err;
-                                });
-                                break;
-                        }
-                        if (errors) {
-                            console.log('Log Error BREAK.');
-                            break;
-                        }
-                    }
-                    if (errors) {
-                        var logs = await updateTaskStatus(access_token, 'error', errors.toString(), task.task_id);
-
-                    } else {
-                        var logs = await updateTaskStatus(access_token, 'completed', 'Task Completed Successfully.', task.task_id);
-                    }
-                    // console.log(logs);
-                    await browser.close()
-                    // console.log(`All done, check the screenshot. ✨`)
-
-                }).catch( async err=>{
-                  // console.log('Error is:' +err);
-                  var logs = await updateTaskStatus(access_token, 'error', err.toString(), task.task_id);
-                  // console.log(logs)
-                })
-
-            }
-            // process.exit();
-
-            // console.log(taskdata);
+            let task_logs = await taskProcess(task, access_token, proxy, args);
+            return task_logs;
         }
-        var logs=await Promise.all(logs);
 
     }
 }
 
 (async () => {
     while (true) {
-        var maincall = await main(access_token);
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+        let project = await getActiveProject(access_token);
+        let mainCall = await main(access_token, project.id);
+        await Promise.all(Object.keys(mainCall));
+        await delay(project.task_delay);
+        // Promise.all(maincall);
     }
-    // var logs=await Promise.all(maincall);
 
 })()
