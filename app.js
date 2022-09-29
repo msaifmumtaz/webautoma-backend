9fd3801b33d02b9ab9c8e1d9e11ceaba91194d67
Muhammad Saif <hello@localhost.localdomain>
2022-09-29 16:19:55 +0500

    Fixed Bugs and add Proxy Region Functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const access_token = 'd0c6e78050da4f11b85bc92aeb21887d';
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
+const apiurl="http://webautoma.pk/api/";
 
 /**
  * Get Active Project Return Project Data of Currently
@@ -10,12 +11,12 @@ puppeteer.use(StealthPlugin());
  * @param {string} access_token_use 
  * @returns {object} Project Data
  */
-async function getActiveProject(access_token_use) {
+async function getActiveProject(access_token_use,apiurl) {
     try {
-        const response = await axios.post('http://webautoma.pk/api/project/get', {
+        const response = await axios.post(apiurl+'project/get', {
             access_token: access_token_use
         });
-        console.log(response.data + 'project');
+        // console.log(response.data + 'project');
         //   var response;
         return response.data;
     } catch (error) {
@@ -30,13 +31,13 @@ async function getActiveProject(access_token_use) {
  * @returns {object} Task Data Object
  */
 
-async function getTaskData(project_id, access_token_use) {
+async function getTaskData(project_id, access_token_use,apiurl) {
     try {
-        const response = await axios.post('http://webautoma.pk/api/project/getTask', {
+        const response = await axios.post(apiurl+'project/getTask', {
             access_token: access_token_use,
             project_id: project_id
         });
-        // console.log(response.data);
+        // console.log(response.data);s
         return response.data;
         //   var response;
     } catch (error) {
@@ -49,12 +50,13 @@ async function getTaskData(project_id, access_token_use) {
  * @param {string} access_token_use 
  * @returns {object} Proxy Data Object
  */
-async function getproxy(access_token_use) {
+async function getproxy(access_token_use, project_id_use,apiurl) {
     try {
-        const response = await axios.post('http://webautoma.pk/api/proxy/get', {
-            access_token: access_token_use
+        const response = await axios.post(apiurl+'proxy/get', {
+            access_token: access_token_use,
+            project_id: project_id_use
         });
-        console.log(response.data);
+        // console.log(response.data);
         return response.data;
         //   var response;
     } catch (error) {
@@ -70,9 +72,9 @@ async function getproxy(access_token_use) {
  * @param {int|string} task_id 
  * @returns {object} Update Status Message Got from Server
  */
-async function updateTaskStatus(access_token_use, status, log_msg, task_id) {
+async function updateTaskStatus(access_token_use, status, log_msg, task_id,apiurl) {
     try {
-        const response = await axios.post('http://webautoma.pk/api/project/updateTaskStatus', {
+        const response = await axios.post(apiurl+'project/updateTaskStatus', {
             access_token: access_token_use,
             task_id: task_id,
             task_status: status,
@@ -86,9 +88,9 @@ async function updateTaskStatus(access_token_use, status, log_msg, task_id) {
     }
 }
 
-async function getTotalTasks(access_token_use, project_id) {
+async function getTotalTasks(access_token_use, project_id,apiurl) {
     try {
-        const response = await axios.post('http://webautoma.pk/api/project/getTotalTasks', {
+        const response = await axios.post(apiurl+'project/getTotalTasks', {
             access_token: access_token_use,
             project_id: project_id,
         });
@@ -100,14 +102,14 @@ async function getTotalTasks(access_token_use, project_id) {
     }
 }
 
-async function taskProcess(task, access_token, proxy, args) {
+async function taskProcess(task, access_token, proxy, args,apiurl) {
     if (task) {
         let puperr;
         const browser = await puppeteer.launch(args).catch(err => {
             puperr = err
         });
         if (puperr) {
-            var logs = await updateTaskStatus(access_token, 'error', puperr.toString(), task.task_id);
+            var logs = await updateTaskStatus(access_token, 'error', puperr.toString(), task.task_id,apiurl);
         }
         try {
             console.log('Running Task Of Project..')
@@ -126,6 +128,7 @@ async function taskProcess(task, access_token, proxy, args) {
             // await page.waitForTimeout();
             // console.log(task.task_data);
             var errors;
+            await page.click('body');
             await page.waitForSelector(task.task_data[0].selector, {
                 visible: true
             });
@@ -176,46 +179,49 @@ async function taskProcess(task, access_token, proxy, args) {
                 }
             }
             if (errors) {
-                var logs = await updateTaskStatus(access_token, 'error', errors.toString(), task.task_id);
+                var logs = await updateTaskStatus(access_token, 'error', errors.toString(), task.task_id,apiurl);
                 // var log=await Promise.all(logser);
 
             } else {
-                var logs = await updateTaskStatus(access_token, 'completed', 'Task Completed Successfully.', task.task_id);
+                var logs = await updateTaskStatus(access_token, 'completed', 'Task Completed Successfully.', task.task_id,apiurl);
             }
-            await page.waitForTimeout(10000);
+            // await page.waitForTimeout(10000);
+            // await page.screenshot({
+            //     path: task.task_id + '.png',
+            //     fullPage: true
+            // })
 
             // console.log(logs);
             await browser.close();
             return logs;
             // console.log(`All done, check the screenshot. ✨`)
         } catch (error) {
-            var logs = await updateTaskStatus(access_token, 'error', error.toString(), task.task_id);
+            var logs = await updateTaskStatus(access_token, 'error', error.toString(), task.task_id,apiurl);
             return logs;
         }
     }
 }
-async function main(access_token, project) {
+async function main(access_token, project, proxy,apiurl) {
     if (project != null || project != '' || project != 0) {
         // var proxy = await getproxy();
-        var proxy = await getproxy(access_token);
         // console.log(proxy);
         if (proxy) {
             if (proxy.use_proxy === 'yes') {
                 proxy_url = proxy.proxy_url;
                 var args = {
                     args: [`--proxy-server=${proxy_url}`],
-                    headless: false
+                    headless: true
                 }
                 var p_username = proxy.proxy_username;
                 var p_password = proxy.proxy_password;
                 console.log('Using Proxy....');
             } else {
                 var args = {
-                    headless: false
+                    headless: true
                 }
             }
-            var task = await getTaskData(project, access_token);
-            let task_logs = await taskProcess(task, access_token, proxy, args);
+            var task = await getTaskData(project, access_token,apiurl);
+            let task_logs = await taskProcess(task, access_token, proxy, args,apiurl);
             return task_logs;
         }
 
@@ -223,13 +229,21 @@ async function main(access_token, project) {
 }
 
 (async () => {
-    while (true) {
+    var lop=true;
+    while (lop) {
         const delay = ms => new Promise(res => setTimeout(res, ms));
-        let project = await getActiveProject(access_token);
-        let mainCall = await main(access_token, project.id);
-        await Promise.all(Object.keys(mainCall));
-        await delay(project.task_delay);
+        let project = await getActiveProject(access_token,apiurl);
+        if (project) {
+            var proxy = await getproxy(access_token, project.id,apiurl);
+            if (proxy && proxy != 'error') {
+                let mainCall = await main(access_token, project.id,proxy,apiurl);
+                await Promise.all(Object.keys(mainCall));
+                await delay(project.task_delay);
+            }
+        }
+
         // Promise.all(maincall);
+
     }
 
 })()
